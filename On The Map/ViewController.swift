94fb7bb57b817a890a43180ccc1ef655fc202af7
t.waner <t.waner@gmail.com>
2015-04-23 20:54:11 -0400

    Initial Commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Taiowa Waner on 4/23/15.
+//  Copyright (c) 2015 Taiowa Waner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
